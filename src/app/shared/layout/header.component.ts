47b0be5e815a037de1719a8aeea25c512ea4f13d
ZiddanDwiPutra <ziddandwiputra21@gmail.com>
2022-04-25 00:21:15 +0700

    add logout icon

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { faSearch, faArrowUp, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSearch,
+  faArrowUp,
+  faUser,
+  faSignOut,
+  faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'header',
@@ -10,6 +16,7 @@ export class HeaderComponent {
   faSearch = faSearch;
   faArrowUp = faArrowUp;
   faUser = faUser;
+  faLogout = faSignOutAlt;
   searchValue = '';
   firstName = 'Waseem';
   lastName = 'Arshad';
